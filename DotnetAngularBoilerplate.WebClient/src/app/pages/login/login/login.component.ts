379b45948c45a72be1c40849736b1ea0eac694c0
Devesh Sahu <devesh.sahu19@outlook.com>
2023-10-23 03:09:37 +0530

    Added social sign in buttons

--- DotnetAngularBoilerplate.WebClient/src/app/pages/login/login/login.component.ts
+++ DotnetAngularBoilerplate.WebClient/src/app/pages/login/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { LoginService } from '../login.service';
 import { NgForm } from '@angular/forms';
+import { PrimeNGConfig } from 'primeng/api';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,20 +11,25 @@ import { NgForm } from '@angular/forms';
 export class LoginComponent implements OnInit {
   isLoading = false;
   isRememberMeChecked = true;
-  constructor(private _loginService: LoginService) {
+  isRememberMeChecked2 = true;
+  constructor(private loginService: LoginService, private primengConfig: PrimeNGConfig) {
 
   }
   ngOnInit(): void {
+    //this.primengConfig.ripple = true;
     console.log(environment.isProd);
 
   }
   onLogin(loginForm: NgForm) {
     this.isLoading = !this.isLoading;
     console.log("onLogin")
-    this._loginService.login(loginForm.value.email, loginForm.value.password).subscribe(result => {
+    this.loginService.login(loginForm.value.email, loginForm.value.password).subscribe(result => {
       console.log(result)
     },
       error => (console.log(error)))
 
+  }
+  forgotPassword(){
+    console.log("forgot password");
   }
 }
